582e7643d94d174f760268ed3a9fd774c27523f5
Chris Jones <chrisjonesdesign85@gmail.com>
2019-08-01 06:54:22 -0400

    installed express and other dependencies, server running, all systems go

---
+++ routes/comments/index.js
@@ -0,0 +1,6 @@
+const commentsRouter = require('express').Router({ mergeParams: true });
+const Posts = require('../../data/db');
+
+
+
+module.exports = commentsRouter;
---
+++ routes/posts/index.js
@@ -0,0 +1,7 @@
+const postsRouter = require('express').Router({ mergeParams: true });
+const Posts = require('../../data/db');
+
+//Get Posts
+
+
+module.exports = postsRouter;
