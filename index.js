5d10b472475cdf56ba042b3870467352a896176e
Chris Jones <chrisjonesdesign85@gmail.com>
2019-08-01 06:24:06 -0400

    got routers scaffolding in place, initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const postsRouter = require('./routes/posts/index');
+const commentsRouter = require('./routes/comments/index')
+const server = express();
+
+
+
+
+
